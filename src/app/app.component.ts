18619a36b46fb798eb7b7c166c13c5dbc8973b75
muthu u <finefixinfotech@gmail.com>
2025-03-19 08:17:43 +0530

    header-landing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+// import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from '../header/header.component';
+import { LandingComponent } from '../landing/landing.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [HeaderComponent,LandingComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'ecomweb';
